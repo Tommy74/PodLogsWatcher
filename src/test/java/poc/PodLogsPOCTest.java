9c61ce871a42e51fd1d2a28f7dac6adcc99a6d6c
Tommaso Borgato <tborgato@tborgato.redhat.com>
2021-06-25 15:30:08 +0200

    added test

---
+++ src/test/java/poc/PodLogsPOCTest.java
@@ -0,0 +1,10 @@
+package poc;
+
+import org.junit.Test;
+
+public class PodLogsPOCTest {
+    @Test
+    public void test() throws InterruptedException {
+        PodLogsPOC.main(new String[]{});
+    }
+}
